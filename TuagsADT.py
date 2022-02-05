5cece4e811e3f31d76e93218d9c1a22b6e323109
adhitia92 <98729482+adhitia92@users.noreply.github.com>
2022-02-05 07:57:00 +0700

    Update TuagsADT.py

--- TuagsADT.py
+++ TuagsADT.py
@@ -1,3 +1,9 @@
+# Assalamualaikum warahmatullahi wabarakatuh, Perkenalkan nama saya Fajar Adhitia Suwandhi dengan NPM G1A021086,
+# disini saya membuat suatu penerapan ADT sederhana berupa menghitung luas Bangun datar, lebih tepatnya menghitung luas segitiga 
+# menggunakan bahasa pemrograman berupa Python, disini alas merupakan suatu perintah untuk memanggil input berupa panjang alas segitia yang akan dimasukan oleh pengguna
+# tinggi merupakan suatu perintah untuk memanggil input berupa panjang segitiga yang akan dimasukan oleh pengguna
+# int(alas)/(tinggi) merupakan mengubah nilai yang ditentukan menjadi integer, dan (*) merupakan simbol perkalian untuk membuat rumus luas segitiga 
+
 alas = input("Panjang Alas Segitiga?(CM) ") 
 tinggi = input("Panjang Tinggi segitiga?(CM) ")
 print("Jadi Luas Segitiga Adalah: ", int(alas) * int(tinggi) * 0.5, "CM2")
